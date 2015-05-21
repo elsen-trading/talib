1dba2ba857d5bd63015958161144ea85b6698563
Daniel Steinberg <dan@elsen.co>
2015-05-20 20:08:51 -0400

    started implementing moving averages

---
+++ MovingAverages.hs
@@ -0,0 +1,25 @@
+import Foreign.C.Types
+import Foreign.Ptr
+
+-- Compile with:
+-- ghc MovingAverages.hs -l ta_lib
+
+foreign import ccall safe "ta_common.h TA_Initialize"
+  c_ta_init :: IO ()
+
+-- just to make sure we're calling the C code
+foreign import ccall safe "ta_func.h TA_ATR_Lookback"
+  c_ta_atr_lookback :: CInt -> CInt
+
+data MaOpts = MaOpts { -- data MaOpts = MaOpts Int
+    maLength :: Int
+  } deriving (Eq, Show)
+
+--ma :: MaOpts -> Vector Double -> Vector Double
+--ma (MaOpts len) vec = 
+
+main :: IO ()
+main = do
+	putStrLn (show (c_ta_atr_lookback 87))
+	putStrLn "Elsen!"
+	
