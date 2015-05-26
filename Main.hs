0189536dbd54048a4a5c6d5839f4bb8bf0a7ecee
Dan <dan@elsen.co>
2015-05-26 12:37:31 -0400

    added relative strength index, and standardized function names, which will work better for TODO: auto-generating the code

--- Main.hs
+++ Main.hs
@@ -22,6 +22,15 @@ type CTA1IntInt1 = CInt        -- startIdx
                 -> Ptr CDouble -- output array
                 -> IO CInt
 
+type CTA1Int1 = CInt        -- startIdx
+             -> CInt        -- endIdx
+             -> Ptr CDouble -- input array
+             -> CInt        -- option
+             -> Ptr CInt    -- outBegIdx
+             -> Ptr CInt    -- outNBElement
+             -> Ptr CDouble -- output array
+             -> IO CInt
+
 -- CTA2_1 - 2 input arrays, no options, and 1 output array
 type CTA2_1 = CInt        -- startIdx
            -> CInt        -- endIdx
@@ -106,6 +115,10 @@ type TS1IntInt = [Double]
               -> Int
               -> TSOutput
 
+type TS1Int = [Double]
+           -> Int
+           -> TSOutput
+
 type TS2_ = [Double]
          -> [Double]
          -> TSOutput
@@ -144,6 +157,7 @@ type TS4_ = [Double]
 
 -- A TSFun is a C ta-lib function, and the corresponding inputs (input data and options)
 data TSFun = TA1IntInt1 CTA1IntInt1 [Double] Int Int
+           | TA1Int1 CTA1Int1 [Double] Int
            | TA2_1 CTA2_1 [Double] [Double]
            | TA4IntInt1 CTA4IntInt1 [Double] [Double] [Double] [Double] Int Int
            | TA2Int2 CTA2Int2 [Double] [Double] Int
@@ -177,6 +191,8 @@ ta_lib tsfun
         rc <- case tsfun of
           TA1IntInt1 fn _ arg1 arg2       -> fn startIdx endIdx cInReal (fromIntegral arg1) (fromIntegral arg2)
                                              cOutBegIdx cOutNbElement cOutReal
+          TA1Int1 fn _ arg1               -> fn startIdx endIdx cInReal (fromIntegral arg1)
+                                             cOutBegIdx cOutNbElement cOutReal
           TA2_1 fn _ _                    -> fn startIdx endIdx cInReal (getInArrPtr 1) cOutBegIdx cOutNbElement
                                              cOutReal
           TA4IntInt1 fn _ _ _ _ arg1 arg2 -> fn startIdx endIdx cInReal (getInArrPtr 1) (getInArrPtr 2) (getInArrPtr 3)
@@ -203,6 +219,7 @@ ta_lib tsfun
           _ -> return $ Left $ fromIntegral rc
     where inReal = map realToFrac $ case tsfun of
             TA1IntInt1 _ in1 _ _             -> in1
+            TA1Int1 _ in1 _                  -> in1
             TA2_1 _ in1 in2                  -> in1 ++ in2
             TA4IntInt1 _ in1 in2 in3 in4 _ _ -> in1 ++ in2 ++ in3 ++ in4
             TA2Int2 _ in1 in2 _              -> in1 ++ in2
@@ -211,6 +228,7 @@ ta_lib tsfun
             TA4_1 _ in1 in2 in3 in4          -> in1 ++ in2 ++ in3 ++ in4
           len = fromIntegral $ length $ case tsfun of
             TA1IntInt1 _ in1 _ _       -> in1
+            TA1Int1 _ in1 _            -> in1
             TA2_1 _ in1 _              -> in1
             TA4IntInt1 _ in1 _ _ _ _ _ -> in1
             TA2Int2 _ in1 _ _          -> in1
@@ -221,6 +239,7 @@ ta_lib tsfun
           endIdx = fromIntegral $ len - 1
           outputs = case tsfun of
             TA1IntInt1 {} -> 1
+            TA1Int1 {}    -> 1
             TA2_1 {}      -> 1
             TA4IntInt1 {} -> 1
             TA2Int2 {}    -> 2
@@ -228,24 +247,24 @@ ta_lib tsfun
             TA3Int1 {}    -> 1
             TA4_1 {}      -> 1
 
+-- TA-Lib : Technical Analysis Library
+
 foreign import ccall unsafe "ta_common.h TA_Initialize"
   c_ta_init :: IO ()
 
--- input: inReal[]; options: int optInTimePeriod, int optInMAType; output: outReal[]
-foreign import ccall unsafe "ta_func.h TA_MA"
-  c_ta_ma :: CTA1IntInt1
+-- AD                  Chaikin A/D Line
 
-taMovingAverage :: TS1IntInt
-taMovingAverage inReal optInTimePeriod optInMAType
-    = ta_lib (TA1IntInt1 c_ta_ma inReal optInTimePeriod optInMAType)
+-- input: inHigh[], inLow[], inClose[], inVolume[];
+-- options: none
+-- output: outReal[]
+foreign import ccall unsafe "ta_func.h TA_AD"
+  c_ta_ad :: CTA4_1
 
--- input: inHigh[], inLow[]; options: none; output: outReal[]
-foreign import ccall unsafe "ta_func.h TA_MEDPRICE"
-  c_ta_medprice :: CTA2_1
+ta_ad :: TS4_
+ta_ad inHigh inLow inClose inVolume
+    = ta_lib (TA4_1 c_ta_ad inHigh inLow inClose inVolume)
 
-taMedianPrice :: TS2_
-taMedianPrice inHigh inLow
-    = ta_lib (TA2_1 c_ta_medprice inHigh inLow)
+-- ADOSC               Chaikin A/D Oscillator
 
 -- input:   inHigh[], inLow[], inClose[], inVolume[];
 -- options: int optInFastPeriod (2-100000), int optInSlowPeriod (2-100000);
@@ -253,9 +272,25 @@ taMedianPrice inHigh inLow
 foreign import ccall unsafe "ta_func.h TA_ADOSC"
   c_ta_adosc :: CTA4IntInt1
 
-taChaikinAdOscillator :: TS4IntInt
-taChaikinAdOscillator inHigh inLow inClose inVolume optInFastPeriod optInSlowPeriod
+ta_adosc :: TS4IntInt
+ta_adosc inHigh inLow inClose inVolume optInFastPeriod optInSlowPeriod
     = ta_lib (TA4IntInt1 c_ta_adosc inHigh inLow inClose inVolume optInFastPeriod optInSlowPeriod)
+
+-- ADX                 Average Directional Movement Index
+
+-- input: inHigh[], inLow[], inClose[];
+-- options: int optInTimePeriod (2-100000)
+-- output: outReal[]
+foreign import ccall unsafe "ta_func.h TA_ADX"
+  c_ta_adx :: CTA3Int1
+
+ta_adx :: TS3Int
+ta_adx inHigh inLow inClose optInTimePeriod
+    = ta_lib (TA3Int1 c_ta_adx inHigh inLow inClose optInTimePeriod)
+
+-- ADXR                Average Directional Movement Index Rating
+-- APO                 Absolute Price Oscillator
+-- AROON               Aroon
 
 -- input:   inHigh[], inLow[];
 -- options: int optInTimePeriod (2-100000);
@@ -263,27 +298,12 @@ taChaikinAdOscillator inHigh inLow inClose inVolume optInFastPeriod optInSlowPer
 foreign import ccall unsafe "ta_func.h TA_AROON"
   c_ta_aroon :: CTA2Int2
 
-taAroon :: TS2Int
-taAroon inHigh inLow optInTimePeriod
+ta_aroon :: TS2Int
+ta_aroon inHigh inLow optInTimePeriod
     = ta_lib (TA2Int2 c_ta_aroon inHigh inLow optInTimePeriod)
 
--- input: inHigh[], inLow[], inClose[], inVolume[];
--- options: int optInTimePeriod (2-100000);
--- output: outReal[]
-foreign import ccall unsafe "ta_func.h TA_MFI"
-  c_ta_mfi :: CTA4Int1
-
-taMoneyFlowIndex :: TS4Int
-taMoneyFlowIndex inHigh inLow inClose inVolume optInTimePeriod
-    = ta_lib (TA4Int1 c_ta_mfi inHigh inLow inClose inVolume optInTimePeriod)
-      
--- input: inReal[], inVolume[]; options: none; output: outReal[]
-foreign import ccall unsafe "ta_func.h TA_OBV"
-  c_ta_obv :: CTA2_1
-
-taOnBalanceVolume :: TS2_
-taOnBalanceVolume inReal inVolume
-    = ta_lib (TA2_1 c_ta_obv inReal inVolume)
+-- AROONOSC            Aroon Oscillator
+-- ATR                 Average True Range
 
 -- input: inHigh[], inLow[], inClose[];
 -- options: int optInTimePeriod (1-100000)
@@ -291,25 +311,187 @@ taOnBalanceVolume inReal inVolume
 foreign import ccall unsafe "ta_func.h TA_ATR"
   c_ta_atr :: CTA3Int1
 
-taAverageTrueRange :: TS3Int
-taAverageTrueRange inHigh inLow inClose optInTimePeriod
+ta_atr :: TS3Int
+ta_atr inHigh inLow inClose optInTimePeriod
     = ta_lib (TA3Int1 c_ta_atr inHigh inLow inClose optInTimePeriod)
-      
--- input: inHigh[], inLow[], inClose[];
--- options: int optInTimePeriod (2-100000)
+
+-- AVGPRICE            Average Price
+-- BBANDS              Bollinger Bands
+-- BETA                Beta
+-- BOP                 Balance Of Power
+-- CCI                 Commodity Channel Index
+-- CDL2CROWS           Two Crows
+-- CDL3BLACKCROWS      Three Black Crows
+-- CDL3INSIDE          Three Inside Up/Down
+-- CDL3LINESTRIKE      Three-Line Strike 
+-- CDL3OUTSIDE         Three Outside Up/Down
+-- CDL3STARSINSOUTH    Three Stars In The South
+-- CDL3WHITESOLDIERS   Three Advancing White Soldiers
+-- CDLABANDONEDBABY    Abandoned Baby
+-- CDLADVANCEBLOCK     Advance Block
+-- CDLBELTHOLD         Belt-hold
+-- CDLBREAKAWAY        Breakaway
+-- CDLCLOSINGMARUBOZU  Closing Marubozu
+-- CDLCONCEALBABYSWALL Concealing Baby Swallow
+-- CDLCOUNTERATTACK    Counterattack
+-- CDLDARKCLOUDCOVER   Dark Cloud Cover
+-- CDLDOJI             Doji
+-- CDLDOJISTAR         Doji Star
+-- CDLDRAGONFLYDOJI    Dragonfly Doji
+-- CDLENGULFING        Engulfing Pattern
+-- CDLEVENINGDOJISTAR  Evening Doji Star
+-- CDLEVENINGSTAR      Evening Star
+-- CDLGAPSIDESIDEWHITE Up/Down-gap side-by-side white lines
+-- CDLGRAVESTONEDOJI   Gravestone Doji
+-- CDLHAMMER           Hammer
+-- CDLHANGINGMAN       Hanging Man
+-- CDLHARAMI           Harami Pattern
+-- CDLHARAMICROSS      Harami Cross Pattern
+-- CDLHIGHWAVE         High-Wave Candle
+-- CDLHIKKAKE          Hikkake Pattern
+-- CDLHIKKAKEMOD       Modified Hikkake Pattern
+-- CDLHOMINGPIGEON     Homing Pigeon
+-- CDLIDENTICAL3CROWS  Identical Three Crows
+-- CDLINNECK           In-Neck Pattern
+-- CDLINVERTEDHAMMER   Inverted Hammer
+-- CDLKICKING          Kicking
+-- CDLKICKINGBYLENGTH  Kicking - bull/bear determined by the longer marubozu
+-- CDLLADDERBOTTOM     Ladder Bottom
+-- CDLLONGLEGGEDDOJI   Long Legged Doji
+-- CDLLONGLINE         Long Line Candle
+-- CDLMARUBOZU         Marubozu
+-- CDLMATCHINGLOW      Matching Low
+-- CDLMATHOLD          Mat Hold
+-- CDLMORNINGDOJISTAR  Morning Doji Star
+-- CDLMORNINGSTAR      Morning Star
+-- CDLONNECK           On-Neck Pattern
+-- CDLPIERCING         Piercing Pattern
+-- CDLRICKSHAWMAN      Rickshaw Man
+-- CDLRISEFALL3METHODS Rising/Falling Three Methods
+-- CDLSEPARATINGLINES  Separating Lines
+-- CDLSHOOTINGSTAR     Shooting Star
+-- CDLSHORTLINE        Short Line Candle
+-- CDLSPINNINGTOP      Spinning Top
+-- CDLSTALLEDPATTERN   Stalled Pattern
+-- CDLSTICKSANDWICH    Stick Sandwich
+-- CDLTAKURI           Takuri (Dragonfly Doji with very long lower shadow)
+-- CDLTASUKIGAP        Tasuki Gap
+-- CDLTHRUSTING        Thrusting Pattern
+-- CDLTRISTAR          Tristar Pattern
+-- CDLUNIQUE3RIVER     Unique 3 River
+-- CDLUPSIDEGAP2CROWS  Upside Gap Two Crows
+-- CDLXSIDEGAP3METHODS Upside/Downside Gap Three Methods
+-- CMO                 Chande Momentum Oscillator
+-- CORREL              Pearson's Correlation Coefficient (r)
+-- DEMA                Double Exponential Moving Average
+-- DX                  Directional Movement Index
+-- EMA                 Exponential Moving Average
+-- HT_DCPERIOD         Hilbert Transform - Dominant Cycle Period
+-- HT_DCPHASE          Hilbert Transform - Dominant Cycle Phase
+-- HT_PHASOR           Hilbert Transform - Phasor Components
+-- HT_SINE             Hilbert Transform - SineWave
+-- HT_TRENDLINE        Hilbert Transform - Instantaneous Trendline
+-- HT_TRENDMODE        Hilbert Transform - Trend vs Cycle Mode
+-- KAMA                Kaufman Adaptive Moving Average
+-- LINEARREG           Linear Regression
+-- LINEARREG_ANGLE     Linear Regression Angle
+-- LINEARREG_INTERCEPT Linear Regression Intercept
+-- LINEARREG_SLOPE     Linear Regression Slope
+-- MA                  All Moving Average
+
+-- input: inReal[]; options: int optInTimePeriod, int optInMAType; output: outReal[]
+foreign import ccall unsafe "ta_func.h TA_MA"
+  c_ta_ma :: CTA1IntInt1
+
+ta_ma :: TS1IntInt
+ta_ma inReal optInTimePeriod optInMAType
+    = ta_lib (TA1IntInt1 c_ta_ma inReal optInTimePeriod optInMAType)
+
+-- MACD                Moving Average Convergence/Divergence
+-- MACDEXT             MACD with controllable MA type
+-- MACDFIX             Moving Average Convergence/Divergence Fix 12/26
+-- MAMA                MESA Adaptive Moving Average
+-- MAX                 Highest value over a specified period
+-- MAXINDEX            Index of highest value over a specified period
+-- MEDPRICE            Median Price
+
+-- input: inHigh[], inLow[]; options: none; output: outReal[]
+foreign import ccall unsafe "ta_func.h TA_MEDPRICE"
+  c_ta_medprice :: CTA2_1
+
+ta_medprice :: TS2_
+ta_medprice inHigh inLow
+    = ta_lib (TA2_1 c_ta_medprice inHigh inLow)
+
+-- MFI                 Money Flow Index
+
+-- input: inHigh[], inLow[], inClose[], inVolume[];
+-- options: int optInTimePeriod (2-100000);
 -- output: outReal[]
-foreign import ccall unsafe "ta_func.h TA_ADX"
-  c_ta_adx :: CTA3Int1
+foreign import ccall unsafe "ta_func.h TA_MFI"
+  c_ta_mfi :: CTA4Int1
 
-taAverageDirectionalMovementIndex inHigh inLow inClose optInTimePeriod
-    = ta_lib (TA3Int1 c_ta_adx inHigh inLow inClose optInTimePeriod)
+ta_mfi :: TS4Int
+ta_mfi inHigh inLow inClose inVolume optInTimePeriod
+    = ta_lib (TA4Int1 c_ta_mfi inHigh inLow inClose inVolume optInTimePeriod)
 
-foreign import ccall unsafe "ta_func.h TA_AD"
-  c_ta_ad :: CTA4_1
+-- MIDPOINT            MidPoint over period
+-- MIDPRICE            Midpoint Price over period
+-- MIN                 Lowest value over a specified period
+-- MININDEX            Index of lowest value over a specified period
+-- MINMAX              Lowest and highest values over a specified period
+-- MINMAXINDEX         Indexes of lowest and highest values over a specified period
+-- MINUS_DI            Minus Directional Indicator
+-- MINUS_DM            Minus Directional Movement
+-- MOM                 Momentum
+-- NATR                Normalized Average True Range
+-- OBV                 On Balance Volume
 
-taChaikinAdLine :: TS4_
-taChaikinAdLine inHigh inLow inClose inVolume
-    = ta_lib (TA4_1 c_ta_ad inHigh inLow inClose inVolume)
+-- input: inReal[], inVolume[]; options: none; output: outReal[]
+foreign import ccall unsafe "ta_func.h TA_OBV"
+  c_ta_obv :: CTA2_1
+
+ta_obv :: TS2_
+ta_obv inReal inVolume
+    = ta_lib (TA2_1 c_ta_obv inReal inVolume)
+
+-- PLUS_DI             Plus Directional Indicator
+-- PLUS_DM             Plus Directional Movement
+-- PPO                 Percentage Price Oscillator
+-- ROC                 Rate of change : ((price/prevPrice)-1)*100
+-- ROCP                Rate of change Percentage: (price-prevPrice)/prevPrice
+-- ROCR                Rate of change ratio: (price/prevPrice)
+-- ROCR100             Rate of change ratio 100 scale: (price/prevPrice)*100
+-- RSI                 Relative Strength Index
+
+-- input: inReal[]; options: int optInTimePeriod (2-100000); output: outReal[]
+foreign import ccall unsafe "ta_func.h TA_RSI"
+    c_ta_rsi :: CTA1Int1
+
+ta_rsi :: TS1Int
+ta_rsi inReal optInTimePeriod
+    = ta_lib (TA1Int1 c_ta_rsi inReal optInTimePeriod)
+
+-- SAR                 Parabolic SAR
+-- SAREXT              Parabolic SAR - Extended
+-- SMA                 Simple Moving Average
+-- STDDEV              Standard Deviation
+-- STOCH               Stochastic
+-- STOCHF              Stochastic Fast
+-- STOCHRSI            Stochastic Relative Strength Index
+-- SUM                 Summation
+-- T3                  Triple Exponential Moving Average (T3)
+-- TEMA                Triple Exponential Moving Average
+-- TRANGE              True Range
+-- TRIMA               Triangular Moving Average
+-- TRIX                1-day Rate-Of-Change (ROC) of a Triple Smooth EMA
+-- TSF                 Time Series Forecast
+-- TYPPRICE            Typical Price
+-- ULTOSC              Ultimate Oscillator
+-- VAR                 Variance
+-- WCLPRICE            Weighted Close Price
+-- WILLR               Williams' %R
+-- WMA                 Weighted Moving Average
 
 terpri :: IO ()
 terpri = putStrLn ""
@@ -331,33 +513,69 @@ main = do
         print close
         terpri
         
-        let open = map (1 +) close
-            
         -- TODO: pass real data into some of these, rather than passing the same series multiple times
+        -- (as is, with unrealistic series, these are just testing that the C calls work and don't e.g. seg fault)
         
-        putStrLn "Moving Average"
-        result <- taMovingAverage close 5 taIntDefault
-        print result
-        terpri
+        let inReal = close
+        let inHigh = map (1 +) close
+        let inLow = map (2 +) close
+        let inClose = map (2 *) close
+        let inVolume = map (fromIntegral . round . (4*)) close
         
-        putStrLn "Median Price"
-        result <- taMedianPrice open close
+        putStrLn "Chaikin A/D Line"
+        result <- ta_ad inHigh inLow inClose inVolume
         print result
         terpri
         
         putStrLn "Chaikin A/D Oscillator"
-        result <- taChaikinAdOscillator open close open close 6 7
+        result <- ta_adosc inHigh inLow inClose inVolume 6 7
+        print result
+        terpri
+        
+        putStrLn "Average Directional Movement Index"
+        result <- ta_adx inHigh inLow inClose 6
         print result
         terpri
         
         putStrLn "Aroon"
-        result <- taAroon open close 5
+        result <- ta_aroon inHigh inLow 5
+        print result
+        terpri
+        
+        putStrLn "Average True Range"
+        result <- ta_atr inHigh inLow inClose 14
+        print result
+        terpri
+        
+        putStrLn "All Moving Average"
+        result <- ta_ma inReal 5 taIntDefault
+        print result
+        terpri
+        
+        putStrLn "Median Price"
+        result <- ta_medprice inHigh inLow
         print result
         terpri
           
         putStrLn "Money Flow Index"
-        result <- taMoneyFlowIndex open close open close 7
+        result <- ta_mfi inHigh inLow inClose inVolume 7
         print result
         terpri
+        
+        putStrLn "On Balance Volume"
+        result <- ta_obv inReal inVolume
+        print result
+        terpri
+        
+        putStrLn "Relative Strength Index"
+        result <- ta_rsi inReal 9
+        print result
+        terpri
+        
+        
+        
+        
+        
+        
         
         
