0635df3cd84a86c2e6063d588e93d40301819a84
Greg Hale <imalsogreg@gmail.com>
2015-05-28 08:06:12 -0400

    update testsuite to match changed Timeseries type

--- tests/Tests/MovingAverage.hs
+++ tests/Tests/MovingAverage.hs
@@ -1,6 +1,7 @@
 module Tests.MovingAverage where
 
 import Control.Applicative ((<$>))
+import Data.Time
 import Test.Tasty
 import Test.Tasty.QuickCheck
 import Test.Tasty.HUnit
@@ -18,12 +19,17 @@ movingAvgTests = testGroup "Moving Averages" [
 
 maKeepsSize :: MAOpt -> Timeseries Double -> Bool
 maKeepsSize opt ts =
-  let len = Vec.length . unTS
+  let len = Vec.length . tsSamps
   in  len ts == len (movingAverage opt ts)
+
+------------------------------------------------------------------------------
+instance (Arbitrary a, Vec.Storable a) => Arbitrary (Timeseries a) where
+  arbitrary = let t0 = UTCTime (fromGregorian 2015 5 1) 0
+                  fq = perMinute 1
+              in  (\xs -> Timeseries (Vec.fromList . take 1000 $ xs) t0 fq)
+                  <$> arbitrary
 
 -- TODO: Forgot, how to properly limit the size of testcase data?
 instance Arbitrary MAOpt where
   arbitrary = MAOpt . (`mod` 1000) <$> arbitrary
 
-instance (Arbitrary a, Vec.Storable a) => Arbitrary (Timeseries a) where
-  arbitrary = Timeseries . Vec.fromList . take 1000 <$> arbitrary
